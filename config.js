a92ff8c93e6648a5e3cd4663ee8b53e01c08422b
PantzCao <95122203+PantzCao@users.noreply.github.com>
2022-03-08 22:10:50 +0700

    Add files via upload
    Creat card

---
+++ config.js
@@ -0,0 +1,10 @@
+const nameGirl = 'Nga';
+const giftUrl = 'https://www.facebook.com/dn.min.T6';
+const eventName = "Happy Women's Day";
+const titleCard = 'Tặng bạn iu';
+const contentCard = 'Chúc bạn iu 08/03 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với bạn iu trong hôm nay và cả những ngày sau';
+
+
+const giftImage = 'Nga.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
